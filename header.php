47bba2a573e1a4a29d1e03809d3b9895a221d2e7
Victor <victor3790@gmail.com>
2021-09-07 18:58:43 -0500

    Menu added

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
                         <a class="navbar-brand text-white" href="index.php">Escritor</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto me-lg-5">
+                            <!--<ul class="navbar-nav ms-auto me-lg-5">
                                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                                 <li class="nav-item"><a class="nav-link" href="biografia.php">Biografía</a></li>
                                 <li class="nav-item dropdown no-caret">
@@ -65,7 +65,15 @@
                                 <li class="nav-item"><a class="nav-link" href="videos.php">Videos</a></li>
                                 <li class="nav-item"><a class="nav-link" href="podcast.php">Podcasts</a></li>
                                 <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
-                            </ul>
+                            </ul>-->
+                            <?php
+                                wp_nav_menu( array(
+                                    'theme_location'    => 'menu-1',
+                                    'container'         => 'ul',
+                                    'menu_class'        => 'navbar-nav ms-auto me-lg-5',
+                                    'fallback_cb'       => false
+                                ) );
+                            ?>
                             <a style="background-color:#876501; border-color:#876501;" class="btn fw-500 ms-lg-4 btn-teal" href="#">
                                 <i class="me-2" data-feather="star"></i>
                                 Membresía
