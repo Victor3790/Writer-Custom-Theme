d718d9bd1f63a3f3173778178c463453e2f3cd06
Victor <victor3790@gmail.com>
2021-09-10 09:34:53 -0500

    Link to blog added

--- template-parts/home-posts.php
+++ template-parts/home-posts.php
@@ -11,7 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) die();
         <div class="row">
             <div class="col-lg-12">
                 <div class="text-left"><h1>Artículos</h1></div>
-                <div class="pb-2 text-end"><a href="#">Ver todos</a></div>
+                <div class="pb-2 text-end">
+                    <a href="<?php echo esc_url( get_post_type_archive_link( 'post' ) ); ?>">
+                        Ver todos
+                    </a>
+                </div>
             </div>
         </div>
         <div class="row pt-1">
