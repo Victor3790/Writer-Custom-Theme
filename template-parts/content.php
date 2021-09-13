cd3be5a2bf6d874324ed58f0c44447169b023b66
Victor <victor3790@gmail.com>
2021-09-13 17:09:29 -0500

    Blog content template added

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,57 +7,47 @@
  * @package Writer_Custom
  */
 
+if ( ! defined( 'ABSPATH' ) ) die();
+
+$author_id = get_the_author_meta('ID');
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				writer_custom_posted_on();
-				writer_custom_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php writer_custom_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'writer-custom' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'writer-custom' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php writer_custom_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<section class="bg-white pb-10">
+    <div class="container pt-5">
+        <div class="d-flex align-items-center justify-content-between">
+            <h1 class="mb-0">Blog</h1>
+        </div>
+        <hr class="mb-4">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-10 col-xl-8">
+                <div class="single-post">
+                    <h2><?php the_title(); ?></h2>
+                    <div class="d-flex align-items-center justify-content-between mb-5">
+                        <div class="single-post-meta me-4">
+                            <img class="single-post-meta-img" src="<?php echo get_avatar_url( $author_id ); ?>" />
+                            <div class="single-post-meta-details">
+                                <div class="single-post-meta-details-name"><?php the_author(); ?></div>
+                                <div class="single-post-meta-details-date"><?php the_date(); ?></div>
+                            </div>
+                        </div>
+                        <div class="single-post-meta-links">
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php esc_url( the_permalink() ); ?>">
+								<i class="fab fa-twitter fa-fw"></i>
+							</a>
+                            <a href="https://twitter.com/intent/tweet?url=<?php esc_url( the_permalink() ); ?>">
+								<i class="fab fa-facebook-f fa-fw"></i>
+							</a>
+                        </div>
+                    </div>
+                    <img class="img-fluid mb-2 rounded" src="<?php esc_url( the_post_thumbnail_url() ); ?>" />
+                    <div class="small text-gray-500 text-center"><?php echo get_the_post_thumbnail_caption(); ?></div>
+                    <div class="single-post-text my-5">
+						<?php the_content(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                        
+</section>
+<hr class="m-0" />
