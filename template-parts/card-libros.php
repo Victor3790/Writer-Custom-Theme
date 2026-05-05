22b84ce4a98cd9a8212f72dc696e6d537e90525f
Victor <victor3790@gmail.com>
2026-05-05 12:45:27 -0600

    Book title added

--- template-parts/card-libros.php
+++ template-parts/card-libros.php
@@ -9,5 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) die();
 <div class="col-6 col-sm-4 col-md-3 col-xl-2 mb-5">
     <a class="card no-boder-radius card-portfolio h-100" href="<?php echo esc_url( the_permalink() ); ?>">
         <img class="no-boder-radius card-img-top" src="<?php echo esc_url( the_post_thumbnail_url() ); ?>">
+        <div class="text-dark fw-bold mt-2"><p class="mb-0"><?php the_title(); ?></p></div>
     </a>
 </div>
